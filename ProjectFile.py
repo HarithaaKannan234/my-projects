670bca04ed1f765460ff24ee509a41d7f7010417
HarithaaKannan <130816672+HarithaaKannan234@users.noreply.github.com>
2023-12-16 20:22:07 +0530

    Update ProjectFile.py

--- ProjectFile.py
+++ ProjectFile.py
@@ -5,15 +5,15 @@ import pandas as pd
 
 question_files = {
     "What are the names of all the videos and their corresponding channels?": "query1.csv",
-    "Which channels have the most number of videos and how many videos do they have?": "C:\\Users\\Admin\\Desktop\\Murdoch\\guvi\\projects\\YoutubeHK\\query2.csv",
-    "What are the top 10 most viewed videos and their respective channels?": "C:\\Users\\Admin\\Desktop\\Murdoch\\guvi\\projects\\YoutubeHK\\query3.csv",
-    "How many comments were made on each video, and what are their corresponding video names?": "C:\\Users\\Admin\\Desktop\\Murdoch\\guvi\\projects\\YoutubeHK\\query4.csv",
-    "Which videos have the highest number of likes, and what are their corresponding channel names?": "C:\\Users\\Admin\\Desktop\\Murdoch\\guvi\\projects\\YoutubeHK\\query5.csv",
-    "What is the total number of likes and dislikes for each video, and what are their corresponding video names?": "C:\\Users\\Admin\\Desktop\\Murdoch\\guvi\\projects\\YoutubeHK\\query6.csv",
-    "What is the total number of views for each channel, and what are their corresponding channel names?": "C:\\Users\\Admin\\Desktop\\Murdoch\\guvi\\projects\\YoutubeHK\\query7.csv",
-    "What are the names of all the channels that have published videos in the year 2022?": "C:\\Users\\Admin\\Desktop\\Murdoch\\guvi\\projects\\YoutubeHK\\query8.csv",
-    "What is the average duration of all videos in each channel, and what are their corresponding channel names?": "C:\\Users\\Admin\\Desktop\\Murdoch\\guvi\\projects\\YoutubeHK\\query9.csv",
-    "Which videos have the highest number of comments, and what are their corresponding channel names?": "C:\\Users\\Admin\\Desktop\\Murdoch\\guvi\\projects\\YoutubeHK\\query10.csv",
+    "Which channels have the most number of videos and how many videos do they have?": "query2.csv",
+    "What are the top 10 most viewed videos and their respective channels?": "query3.csv",
+    "How many comments were made on each video, and what are their corresponding video names?": "query4.csv",
+    "Which videos have the highest number of likes, and what are their corresponding channel names?": "query5.csv",
+    "What is the total number of likes and dislikes for each video, and what are their corresponding video names?": "query6.csv",
+    "What is the total number of views for each channel, and what are their corresponding channel names?": "query7.csv",
+    "What are the names of all the channels that have published videos in the year 2022?": "query8.csv",
+    "What is the average duration of all videos in each channel, and what are their corresponding channel names?": "query9.csv",
+    "Which videos have the highest number of comments, and what are their corresponding channel names?": "query10.csv",
 }
 
 # Title of the Streamlit app
